477ef7127a0d4b24d94974b592f5e305ba48ee00
Raunit Verma <ramgopal10258@gmail.com>
2022-01-07 12:25:41 +0530

    first

--- HotelManagement/src/HotelManagementPackage/DashBoard.java
+++ HotelManagement/src/HotelManagementPackage/DashBoard.java
@@ -1,40 +1,83 @@
 package HotelManagement.src.HotelManagementPackage;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class DashBoard extends JFrame implements ActionListener {
 
-
+    private JFrame frame;
     DashBoard(){
-//        setSize(500,500);
-        setTitle("Hotel Management by Raunit Verma");
-        setBounds(200,100,500,500);
+////        setSize(500,500);
+//        setTitle("Hotel Management by Raunit Verma");
+//        setBounds(200,100,500,500);
+//
+//        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("HotelManagement/src/icons/hotel.jpg"));
+//        JLabel l1=new JLabel(i1);
+////        l1.setBounds(0,0,500,500);
+//        add(l1);
+//        JButton reception = new JButton("Reception");
+//        add(reception);
+//        reception.setBounds(100,100,300,30);
+//        setLayout(null);
+//        setBounds(0,0,1000,1000);
+//        setVisible(true);
+//        reception.addActionListener((ActionListener)this );
+//        JButton addrooms = new JButton("Add Rooms");
+//        add(addrooms);
+//        addrooms.setBounds(420,100,300,30);
+//        addrooms.addActionListener((ActionListener)this );
+//        JButton Employee = new JButton("Add Employee");
+//        add(Employee);
+//        Employee.setBounds(420,140,300,30);
+//        Employee.addActionListener((ActionListener)this );
 
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("HotelManagement/src/icons/hotel.jpg"));
-        JLabel l1=new JLabel(i1);
-//        l1.setBounds(0,0,500,500);
-        add(l1);
-        JButton reception = new JButton("Reception");
-        add(reception);
-        reception.setBounds(100,100,300,30);
-        setLayout(null);
-        setBounds(0,0,1000,1000);
-        setVisible(true);
-        reception.addActionListener((ActionListener)this );
-        JButton addrooms = new JButton("Add Rooms");
-        add(addrooms);
-        addrooms.setBounds(420,100,300,30);
-        addrooms.addActionListener((ActionListener)this );
-        JButton Employee = new JButton("Add Employee");
-        add(Employee);
-        Employee.setBounds(420,140,300,30);
-        Employee.addActionListener((ActionListener)this );
+        frame = new JFrame();
+        frame.setBounds(100, 100, 1037, 637);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        JButton btnNewButton = new JButton("Reception");
+        btnNewButton.setFont(new Font("Consolas", Font.BOLD, 25));
+        btnNewButton.setBounds(126, 165, 300, 50);
+        frame.getContentPane().add(btnNewButton);
+        btnNewButton.addActionListener((ActionListener)this );
+        JButton btnAddEmployee = new JButton("Add Employee");
+        btnAddEmployee.setFont(new Font("Consolas", Font.BOLD, 25));
+        btnAddEmployee.setBounds(619, 165, 300, 50);
+        frame.getContentPane().add(btnAddEmployee);
+        btnAddEmployee.addActionListener((ActionListener)this );
+        JButton btnAddRooms = new JButton("Add Rooms");
+        btnAddRooms.setFont(new Font("Consolas", Font.BOLD, 25));
+        btnAddRooms.setBounds(619, 288, 300, 50);
+        frame.getContentPane().add(btnAddRooms);
+        btnAddRooms.addActionListener((ActionListener)this );
+        JLabel lblNewLabel_1 = new JLabel("Welcome To RV Group of Hotels");
+        lblNewLabel_1.setForeground(Color.YELLOW);
+        lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel_1.setFont(new Font("Open Sans", Font.BOLD, 35));
+        lblNewLabel_1.setBounds(126, 23, 834, 57);
+        frame.getContentPane().add(lblNewLabel_1);
+
+        JLabel lblNewLabel = new JLabel("");
+        lblNewLabel.setIcon(new ImageIcon("D:\\Java\\HotelManagement\\src\\icons\\Atlantis-The-Palm__2018_Zero-Entry-1.jpg"));
+        lblNewLabel.setBounds(-50, -17, 1357, 815);
+        frame.getContentPane().add(lblNewLabel);
+        frame.setVisible(true);
     }
 
     public static void main(String Args[]){
-        new DashBoard();
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    DashBoard window = new DashBoard();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
